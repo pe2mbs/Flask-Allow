32b5a7b1b5b5c732179fdd3aecd8977efc76d547
Marc Bertens - Administrator <m.bertens@pe2mbs.nl>
2023-12-03 08:51:31 +0100

    Fining the tests so that they run on github

--- tests/test_flask_allow.py
+++ tests/test_flask_allow.py
@@ -40,12 +40,12 @@ def test_validate_address():
     assert( FlaskAllow.networkMask( '192.168.110.123' ) )
     assert( FlaskAllow.networkMask( '2001:0db8:85a3:0000:0000:8a2e:0370:7334' ) )
     assert( FlaskAllow.networkMask( '192.168.110.0' ) )
-    assert( FlaskAllow.networkMask( 'matrix' ) )
+    assert( FlaskAllow.networkMask( 'example.org' ) )
     assert( FlaskAllow.networkMask( 'localhost' ) )
     assert( FlaskAllow.networkMask( "2001:db00::0/24" ) )
     assert( FlaskAllow.networkMask( '192.168.110.0/24' ) )
     with pytest.raises(ipaddress.AddressValueError):
-        assert( FlaskAllow.networkMask( 'sukkel.exambple.nl' ) )
+        assert( FlaskAllow.networkMask( 'notexists.example.org' ) )
 
     return
 
